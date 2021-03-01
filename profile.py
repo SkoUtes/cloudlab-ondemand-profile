c4e72c42e2cd539d81551cf28c8d47dc024bf88c
root <root@utah-dev1.chpc.utah.edu>
2021-03-01 16:35:06 -0700

    editing install.sh

--- profile.py
+++ profile.py
@@ -1,22 +1,22 @@
 #!/usr/local/python3
 
+# Import the Portal object.
 import geni.portal as portal
-import geni.rspec.pg as pg
-import geni.rspec.emulab as emulab
-import geni.respec.igext as igext
+# Import the ProtoGENI library.
+import geni.rspec.pg as rspec
 
-pc = portal.Context()
+# Create a request object
+request = portal.context.makeRequestRSpec()
 
-request = pc.makeRequestRSpec()
-
+# Add a raw PC to the request
 node = request.RawPC("Open_OnDemand")
 
 # Request an image for this node
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS7-64-STD"
 
 # Install and execute startup scripts
-node.addService(rspec.Install())
+node.addService(rspec.Execute(shell="bash", command="/local/repository/install.sh"))
 
-pc.printRequestRSpec()
+portal.context.printRequestRSpec()
 
 
