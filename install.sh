08f631c6a5df805a230a76070ac8d655a3711c28
root <root@utah-dev1.chpc.utah.edu>
2021-03-03 16:12:45 -0700

    changing install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo yum update -y
-touch /home/test
-sudo yum install -y epel-release
-sudo yum install -y centos-release-scl subscription manager
+sudo yum update -y 
+sleep 10
+sudo yum install -y epel-release centos-release-scl subscription manager
+sleep 10
 sudo yum install -y https://yum.osc.edu/ondemand/1.8/ondemand-release-web-1.8-1.noarch.rpm
-sudo yum install -y ondemand
-sudo yum install -y ondemand-selinux
+sleep 10 
+sudo yum install -y ondemand ondemand-selinux
 (cd /opt && wget https://downloads.jboss.org/keycloak/9.0.0/keycloak-9.0.0.tar.gz && tar xzf keycloak-9.0.0.tar.gz)
 sudo groupadd -r keycloak && useradd -m -d /var/lib/keycloak -s /sbin/nologin -r -g keycloak keycloak)
 (cd /opt && sudo chown keycloak: -R keycloak-9.0.0)
