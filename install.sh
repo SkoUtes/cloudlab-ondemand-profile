ec92bc1ad55b96914804be2b2de952c5061bb046
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-03-05 11:14:40 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
+#Install Open OnDemand components
 yum update -y 
 sleep 10
-yum install -y epel-release centos-release-scl subscription manager
+yum install -y epel-release centos-release-scl subscription-manager
 sleep 10
 yum install -y https://yum.osc.edu/ondemand/1.8/ondemand-release-web-1.8-1.noarch.rpm
 sleep 10 
@@ -21,3 +23,58 @@ groupadd -r keycloak && useradd -m -d /var/lib/keycloak -s /sbin/nologin -r -g k
 chown keycloak: -R keycloak-9.0.0
 cd /opt/keycloak-9.0.0 
 sudo -u keycloak chmod 0700 standalone
+yum install java-1.8.0-openjdk-devel
+#Generate admin user
+export KC_PASSWORD=$(opensssl rand -hex 20) && echo $KC_PASSWORD >> /root/kc-password.txt
+sudo -u keycloak ./bin/add-user-keycloak.sh --user admin --password $KC_PASSWORD --realm master
+#Enable proxying to keycloak
+sudo -u keycloak ./bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=proxy-address-forwarding,value=true)'
+sudo -u keycloak ./bin/jboss-cli.sh 'embed-server,/socket-binding-group=standard-sockets/socket-binding=proxy-https:add(port=443)'
+sudo -u keycloak ./bin/jboss-cli.sh 'embed-server,/subsystem=undertow/server=default-server/http-listener=default:write-attribute(name=redirect-socket,value=proxy-https)'
+#Create keycloak service
+sudo cat > /etc/systemd/system/keycloak.service <<EOF
+
+[Unit]
+Description=Jboss Application Server
+After=network.target
+
+[Service]
+Type=idle
+User=keycloak
+Group=keycloak
+ExecStart=/opt/keycloak-9.0.0/bin/standalone.sh -b 0.0.0.0
+TimeoutStartSec=600
+TimeoutStopSec=600
+
+[Install]
+WantedBy=multi-user.target
+EOF
+#Start up keycloak
+systemctl daemon-reload
+systemctl start keycloak
+sleep 5
+#Enable proxying to keycloak
+#cat > /opt/rh/httpd24/root/etc/httpd/conf.d/ood-keycloak.conf <<EOF
+#<VirtualHost *:443>
+#  ServerName ondemand-idpdev.hpc.osc.edu
+#
+#  ErrorLog  "logs/keycloak_error_ssl.log"
+#  CustomLog "logs/keycloak_access_ssl.log" combined
+#
+#  SSLEngine On
+#  SSLCertificateFile "/etc/pki/tls/certs/webdev07.hpc.osc.edu.crt"
+#  SSLCertificateKeyFile "/etc/pki/tls/private/webdev07.hpc.osc.edu.key"
+#  SSLCertificateChainFile "/etc/pki/tls/certs/webdev07.hpc.osc.edu-interm.crt"
+#
+#  # Proxy rules
+#  ProxyRequests Off
+#  ProxyPreserveHost On
+#  ProxyPass / http://localhost:8080/
+#  ProxyPassReverse / http://localhost:8080/
+#
+#  ## Request header rules
+#  ## as per http://httpd.apache.org/docs/2.2/mod/mod_headers.html#requestheader
+#  RequestHeader set X-Forwarded-Proto "https"
+#  RequestHeader set X-Forwarded-Port "443"
+#</VirtualHost>
+#EOF
