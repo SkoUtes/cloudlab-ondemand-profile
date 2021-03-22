b86c5738b41d9b2d0386e93adf4e9eba167500fa
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-03-22 14:28:48 -0600

    Update profile.py

--- profile.py
+++ profile.py
@@ -28,8 +28,8 @@ node2.hardware_type = "m400"
 link1 = request.Link(members = [node1, node2])
 
 # Request an image for this node
-node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS7-64-STD"
-node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS7-64-STD"
+node1.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
+node2.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
 
 # Install and execute startup scripts
 node1.addService(rspec.Execute(shell="sh", command="sudo -u root /local/repository/ondemand.sh"))
