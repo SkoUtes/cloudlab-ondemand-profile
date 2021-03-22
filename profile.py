9a864b38b87f1151c123c5892b8afde5c2224f5e
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-03-22 14:35:39 -0600

    Update profile.py

--- profile.py
+++ profile.py
@@ -18,22 +18,18 @@ request = portal.context.makeRequestRSpec()
 
 # Create two nodes
 node1 = request.RawPC("node1")
-node2 = request.RawPC("node2")
 
 # Set each of the two to specifically request "m400" nodes, which in CloudLab, are ARM
 node1.hardware_type = "m400"
-node2.hardware_type = "m400"
 
 # Create a link between them
 link1 = request.Link(members = [node1, node2])
 
 # Request an image for this node
-node1.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
-node2.disk_image = 'urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU18-64-STD'
+node1.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS7-64-STD"
 
 # Install and execute startup scripts
 node1.addService(rspec.Execute(shell="sh", command="sudo -u root /local/repository/ondemand.sh"))
-node2.addService(rspec.Execute(shell="sh", command="sudo -u root /local/repository/keycloak.sh"))
 
 portal.context.printRequestRSpec()
 
