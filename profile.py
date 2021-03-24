fab0258aae9959b410ffebb85ae351613b40a9bf
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-03-24 15:06:55 -0600

    Update profile.py

--- profile.py
+++ profile.py
@@ -28,10 +28,12 @@ node2.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//CENTOS7-64-STD
 
 # Install and execute startup scripts
 node1.addService(rspec.Execute(shell="sh", command="sudo -u root chmod 700  \
-	/local/repository/automated_scripts/ondemand.sh && chmod 700 /local/repository/ondemand_config.sh && \
-	/local/repository/automated_scripts/ondemand.sh"))
+	/local/repository/automated_scripts/ondemand.sh && \
+	sudo -u rootchmod 700 /local/repository/ondemand_config.sh && \
+	sudo -u root /local/repository/automated_scripts/ondemand.sh"))
 node2.addService(rspec.Execute(shell="sh", command="sudo -u root chmod 700 \
-	/local/repository/automated_scripts/keycloak.sh && chmod 700 /local/repository/keycloak_config.sh && \
-	/local/repository/automated_scripts/keycloak.sh"))
+	/local/repository/automated_scripts/keycloak.sh && \
+	sudo -u root chmod 700 /local/repository/keycloak_config.sh && \
+	sudo -u root /local/repository/automated_scripts/keycloak.sh"))
 
 portal.context.printRequestRSpec()
