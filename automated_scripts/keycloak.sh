953349f88be6cd47b2b2405b0c58384dca3c358c
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-03-24 15:24:23 -0600

    moving certbot pieces to keycloak_config

--- automated_scripts/keycloak.sh
+++ automated_scripts/keycloak.sh
@@ -66,33 +66,3 @@ systemctl enable --now snapd.socket && ln -s /var/lib/snapd/snap /snap && \
 snap install core ; snap install core ; snap refresh core
 snap install --classic certbot ; snap install --classic certbot
 ln -s /snap/bin/certbot /usr/bin/certbot
-
-# Run certbot
-certbot --apache -d $hostname --agree-tos -m u1064657@umail.utah.edu
-
-# Place apache in front of keycloak
-cat > /etc/httpd/conf.d/ood-keycloak.conf <<EOF
-<VirtualHost *:443>
-  ServerName $hostname
-
-  ErrorLog  "/var/log/httpd/error_log"
-  CustomLog "/var/log/httpd/access_log" combined
-
-  SSLEngine on
-  SSLCertificateFile "/etc/letsencrypt/live/$hostname/cert.pem"
-  SSLCertificateKeyFile "/etc/letsencrypt/live/$hostname/privkey.pem"
-  SSLCACertificatePath    "/etc/letsencrypt/live/$hostname"
-  Include "/etc/letsencrypt/options-ssl-apache.conf"
-
-  ProxyRequests Off
-  ProxyPreserveHost On
-  ProxyPass / http://localhost:8080/
-  ProxyPassReverse / http://localhost:8080/
-
-  RequestHeader set X-Forwarded-Proto "https"
-  RequestHeader set X-Forwarded-Port "443"
-</VirtualHost>
-EOF
-
-# Restart keycloak and apache
-systemctl restart httpd keycloak
--- keycloak_config.sh
+++ keycloak_config.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
+# Run certbot
+certbot --apache -d $hostname --agree-tos -m u1064657@umail.utah.edu
+
+# Place apache in front of keycloak
+cat > /etc/httpd/conf.d/ood-keycloak.conf <<EOF
+<VirtualHost *:443>
+  ServerName $hostname
+
+  ErrorLog  "/var/log/httpd/error_log"
+  CustomLog "/var/log/httpd/access_log" combined
+
+  SSLEngine on
+  SSLCertificateFile "/etc/letsencrypt/live/$hostname/cert.pem"
+  SSLCertificateKeyFile "/etc/letsencrypt/live/$hostname/privkey.pem"
+  SSLCACertificatePath    "/etc/letsencrypt/live/$hostname"
+  Include "/etc/letsencrypt/options-ssl-apache.conf"
+
+  ProxyRequests Off
+  ProxyPreserveHost On
+  ProxyPass / http://localhost:8080/
+  ProxyPassReverse / http://localhost:8080/
+
+  RequestHeader set X-Forwarded-Proto "https"
+  RequestHeader set X-Forwarded-Port "443"
+</VirtualHost>
+EOF
+
+# Restart keycloak and apache
+systemctl restart httpd keycloak
+
 # Place apache in front of keycloak
 cat > /etc/httpd/conf.d/ood-keycloak.conf <<EOF
 <VirtualHost *:443>
