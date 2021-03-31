83ff2be5bf009f4b5d6c84a17d0127664fa6cea8
SkoUtes <53663229+SkoUtes@users.noreply.github.com>
2021-03-31 15:55:44 -0600

    Update kill_script.sh

--- kill_script.sh
+++ kill_script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-while
+sleep 20
+
+while :
+do
+	sleep 2
+	if [[ tail /local/logs/install.log ]]; then
+		break
+	fi
+done
 ps aux|grep yum update|cut -c -25 |grep -E -o '[0-9]{3,5}'
